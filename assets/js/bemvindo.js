45ebc9cd305ca023354597f56bb210afc4560e68
Emerson <emersonjuniormt@gmail.com>
2023-03-28 22:29:38 -0300

    Criando A pagina empresa dois, e programando alguns elementos de click

--- assets/js/bemvindo.js
+++ assets/js/bemvindo.js
@@ -1,24 +1,30 @@
-
 const selectedClass = 'selected';
+const btnContinuar = document.getElementById('continuar');
 
+document.addEventListener('click', (event) => {
+  const cliente = document.querySelector('.cliente');
+  const clientetex = document.getElementById('text-cliente');
+  const imgcliente = document.getElementById('img-cliente');
+  const empresa = document.querySelector('.empresa');
+  const empresatext = document.getElementById('text-empresa');
+  const imgempresa = document.getElementById('img-emp');
 
-const selecionar = document.addEventListener('click', (event) => {
-    const cliente = document.querySelector('.cliente');
-    const clientetex = document.getElementById('text-cliente');
-    const imgcliente = document.getElementById('img-cliente');
-    const empresa = document.querySelector('.empresa');
-    const empresatext = document.getElementById('text-empresa');
-    const imgempresa = document.getElementById('img-emp');
-    
-    
-    if (event.target === cliente ||  event.target === clientetex || event.target === imgcliente   ) {
-        empresa.classList.remove(selectedClass);
-        cliente.classList.add(selectedClass);
-    }
-    
-    else if (event.target === empresa || event.target === empresatext || event.target === imgempresa ) {
-        cliente.classList.remove(selectedClass);
-    
-        empresa.classList.add(selectedClass);
+  if (event.target === cliente || event.target === clientetex || event.target === imgcliente) {
+    empresa.classList.remove(selectedClass);
+    cliente.classList.add(selectedClass);
+  } else if (event.target === empresa || event.target === empresatext || event.target === imgempresa) {
+    cliente.classList.remove(selectedClass);
+    empresa.classList.add(selectedClass);
+  }
+});
+
+btnContinuar.addEventListener('click', () => {
+  const cliente = document.querySelector('.cliente');
+  const empresa = document.querySelector('.empresa');
+
+  if (cliente.classList.contains(selectedClass)) {
+    window.location.href = "tela1.html";
+  }else if(empresa.classList.contains(selectedClass)){
+    window.location.href = "empresa2.html";
   }
 });
